de6cbb7f6000c023edbe609e288217ec0ba8e1a4
Eddikos <eldar.alasgarov@hotmail.com>
2017-11-12 10:04:36 +0200

    A test for a Deck class method and comments

--- app/src/test/java/it/polimi/ma/group07/briscola/ExampleUnitTest.java
+++ app/src/test/java/it/polimi/ma/group07/briscola/ExampleUnitTest.java
@@ -12,8 +12,10 @@ import java.util.Arrays;
 import it.polimi.ma.group07.briscola.model.Brain;
 import it.polimi.ma.group07.briscola.model.Briscola;
 import it.polimi.ma.group07.briscola.model.Card;
+import it.polimi.ma.group07.briscola.model.Deck;
 import it.polimi.ma.group07.briscola.model.Exceptions.InvalidCardDescriptionException;
 import it.polimi.ma.group07.briscola.model.Exceptions.InvalidGameStateException;
+import it.polimi.ma.group07.briscola.model.Exceptions.NoCardInDeckException;
 import it.polimi.ma.group07.briscola.model.Parser;
 import it.polimi.ma.group07.briscola.model.State;
 import it.polimi.ma.group07.briscola.model.StateBundle;
@@ -171,6 +173,12 @@ public class ExampleUnitTest {
         System.out.println("Total valid States:"+count);
     }
 
+    /**
+     * Check of the determineWinner() method in the Brain class
+     * that determines the winning card on the surface
+     * @throws InvalidCardDescriptionException
+     */
+
     @Test
     public void checkDeterminedCardWinner() throws InvalidCardDescriptionException {
         Brain br = new Brain();
@@ -194,6 +202,11 @@ public class ExampleUnitTest {
         }
     }
 
+    /**
+     * Check of the calculatePoints() method in the Brain class
+     * that calculates total points of cards on the surfaceee
+     * @throws InvalidCardDescriptionException
+     */
     @Test
     public void checkCalculatedPoints() throws InvalidCardDescriptionException {
         Brain br = new Brain();
@@ -215,6 +228,11 @@ public class ExampleUnitTest {
         }
     }
 
+    /**
+     * Check the Card constructor in the Card class
+     * by providing it a set of all possible Invalid pairs of characters for card creation
+     * @throws Exception
+     */
     @Test
     public void checkCard() throws Exception {
         Brain br = new Brain();
@@ -260,6 +278,23 @@ public class ExampleUnitTest {
             }
             catch(Exception e){}
         }
+    }
+
+    /**
+     * Check drawCard() method from the Deck class
+     * and pass an Empty Deck to it and check if you can draw a Card from it.
+     * @throws NoCardInDeckException
+     */
+    @Test
+    public void checkDrawCard() throws NoCardInDeckException, InvalidCardDescriptionException {
+        Deck d = new Deck("");
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        try{
+            d.drawCard();
+            fail("Drew a card from an empty Deck");
+        }
+        catch(Exception e){}
     }
 
     private String generateState(){
